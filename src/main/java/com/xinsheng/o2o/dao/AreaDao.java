7f26f2cfc9dc0a98b7ccf0d62ab9669b70ddcdf3
CoffeeCode <977496475@qq.com>
2022-10-27 13:51:35 +0800

    解决bug，继续开发

--- src/main/java/com/xinsheng/o2o/dao/AreaDao.java
+++ src/main/java/com/xinsheng/o2o/dao/AreaDao.java
@@ -1,10 +1,14 @@
 package com.xinsheng.o2o.dao;
-
+import  com.xinsheng.o2o.entity.Area;
 import java.util.List;
 
 import com.xinsheng.o2o.entity.Area;
 
 public interface AreaDao {
 
-	List <Area> queryArea();
+	/*
+	 * 列出区域列表
+	 * @return areaList
+	 */
+	List<Area> queryArea();
 }
--- src/test/java/com/xinsheng/o2o/dao/AreaDaoTest.java
+++ src/test/java/com/xinsheng/o2o/dao/AreaDaoTest.java
@@ -1,10 +1,9 @@
 package com.xinsheng.o2o.dao;
-
+import com.xinsheng.o2o.BaseTest;
+import com.xinsheng.o2o.entity.Area;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.Assert.assertEquals;
-import com.xinsheng.o2o.BaseTest;
-import com.xinsheng.o2o.entity.Area;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +15,7 @@ public class AreaDaoTest extends BaseTest {
 	
 	@Test
 	public void testQueryArea() {
-		
+		List<Area>areaList = areaDao.queryArea();
 		assertEquals(2,areaList.size());
 		
 	}

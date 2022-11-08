351da9da1a69c6725e275ccbb5df2e292cbf62ff
CoffeeCode <977496475@qq.com>
2022-11-08 21:47:19 +0800

    ShopManagementController逻辑

--- src/main/java/com/xinsheng/o2o/util/HttpServletRequestUtil.java
+++ src/main/java/com/xinsheng/o2o/util/HttpServletRequestUtil.java
@@ -3,60 +3,54 @@ package com.xinsheng.o2o.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class HttpServletRequestUtil {
+	public static int getInt(HttpServletRequest request, String name) {
 
-	public static int getInt(HttpServletRequest request , String key) {
 		try {
-			return Integer.decode(request.getParameter(key));//将key转化为整型
-		}catch (Exception e) {
-			return -1;
-		}	
-	}
-	public static long getLong(HttpServletRequest request , String key) {
-		try {
-			return Long.valueOf(request.getParameter(key));//将key转化为整型
-		}catch (Exception e) {
+			return Integer.decode(request.getParameter(name));
+		} catch (Exception e) {
 			return -1;
 		}
 	}
-	public static Double getDouble(HttpServletRequest request , String key) {
+
+	public static long getLong(HttpServletRequest request, String name) {
+
 		try {
-			return Double.valueOf(request.getParameter(key));//将key转化为整型
-		}catch (Exception e) {
+			return Long.valueOf(request.getParameter(name));
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+
+	public static Double getDouble(HttpServletRequest request, String name) {
+
+		try {
+			return Double.valueOf(request.getParameter(name));
+		} catch (Exception e) {
 			return -1d;
 		}
 	}
-	
-	public static boolean getBoolean(HttpServletRequest request , String key) {
+
+	public static Boolean getBoolean(HttpServletRequest request, String name) {
+
 		try {
-			return Boolean.valueOf(request.getParameter(key));//将key转化为整型
-		}catch (Exception e) {
+			return Boolean.valueOf(request.getParameter(name));
+		} catch (Exception e) {
 			return false;
 		}
 	}
-	
-	public static String getString(HttpServletRequest request , String key) {
+
+	public static String getString(HttpServletRequest request, String name) {
 		try {
-			String result = request.getParameter(key);
+			String result = request.getParameter(name);
 			if (result != null) {
-				result =result.trim(); 
+				result = result.trim();
 			}
-			if ("".equals(result)) {
+			if ("".equals(result))
 				result = null;
-			}
 			return result;
-			
 		} catch (Exception e) {
 			return null;
 		}
-		
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	
-	
 }
--- src/main/java/com/xinsheng/o2o/web/shopadmin/ShopManagementController.java
+++ src/main/java/com/xinsheng/o2o/web/shopadmin/ShopManagementController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xinsheng.o2o.entity.Shop;
@@ -24,7 +27,7 @@ public class ShopManagementController {
 		
 		//1.接受并转换响应的参数，包括店铺信息及图片信息
 		Map<String,Object> modelMap=new HashMap<String,Object>();
-		String shopStr = new HttpServletRequestUtil.getString(request,"shopStr");
+		String shopStr =  HttpServletRequestUtil.getString(request,"shopStr");
 		ObjectMapper mapper = new ObjectMapper();
 		Shop shop = null;
 		try {
@@ -35,9 +38,32 @@ public class ShopManagementController {
 			return modelMap;
 		}
 		
+		CommonsMultipartFile shopImg = null;
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		if (commonsMultipartResolver.isMultipart(request)) {
+			MultipartHttpServletRequest multipartHttpServletRequest  = (MultipartHttpServletRequest) request;
+			shopImg = (CommonsMultipartFile) multipartHttpServletRequest
+					.getFile("shopImg");
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "上传图片不能为空");
+			return modelMap;
+		}
+		try {
+			shop = mapper.readValue(shopStr, Shop.class);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.toString());
+			return modelMap;
+		}
+		
+	
+		
 		//2.注册店铺
 		//3.返回结果,
 
-		
+
+		return modelMap;
 	}
 }
